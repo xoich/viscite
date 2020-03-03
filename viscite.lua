7070c38a82b0982c695075a4be375eba3712dc4a
mistnim <daniele@parisi.xyz>
2020-03-03 14:42:06 +0100

    adding more commands

--- viscite.lua
+++ viscite.lua
@@ -1,31 +1,82 @@
-state = 'n'
-
-local function key(code, shift, ctrl, alt)
-	local mod = ((shift and 1 or 0) << 2) | ((ctrl and 1 or 0) << 1) | (alt and 1 or 0)
-	if type(code) == "string" then code = string.byte(code) end
-	return (code << 3) | mod
+local function keycode(code, shift, ctrl, alt)
+	return ((shift and 1 or 0) << 2) | ((ctrl and 1 or 0) << 1) | (alt and 1 or 0)
+		| (code << 3)
 end
 
+local function keych(ch, shift, ctrl, alt)
+	return keycode(string.byte(ch), shift, ctrl, alt)
+end
+
+local function iMode()
+	editor.CaretStyle = 1
+	OnKey = imapOnKey
+end
+
+local function nMode()
+	editor.CaretStyle = 2
+	OnKey = nmapOnKey
+end
+
+local function OnKeyFromMap(map)
+	OnKey = function (key, s, c, a)
+		local act = map[keycode(key, s, c, a)]
+		if act then
+			local morekeys = false
+			if type(act) == "string" then
+				morekeys = editor[act](editor)
+			else
+				act()
+			end
+			if not morekeys then nMode() end
+		else
+			print "Keybinding not found"
+			nMode()
+		end
+		return true
+	end
+end
+
+imap = {
+	[keych('h', false, true)] = "DeleteBack",
+	[keych('[', false, true)] = nMode,
+	[keych('m', false, true)] = "NewLine",
+	[keych('w', false, true)] = "DelWordLeft",
+	[keycode(65307)] = nMode
+}
+
+local dmap = {
+	[keych'w'] = function() editor:DelWordRight() end,
+	[keych'd'] = function() editor:LineDelete() end
+}
+
 nmap = {
-	[key'0'] = "HomeDisplay",
-	[key'k'] = "LineUp", 
-	[key'j'] = "LineDown", 
-	[key'b'] = "WordLeft", 
-	[key'w'] = "WordRight", 
-	[key'p'] = "Paste", 
-	[key'$'] = "LineEnd",
-	[key'h'] = "CharLeft",
-	[key'l'] = "CharRight",
-	[key'u'] = "Undo",
-	[key('r',false, true)] = "Redo",
-	[key('d',false, true)] = "PageDown",
-	[key('u',false, true)] = "PageUp",
-	[key'i'] = function() OnKey = imapOnKey end
+	[keych'd'] = function() OnKeyFromMap(dmap) end,
+	[keych'g'] = function () OnKeyFromMap({[keych'g'] = "DocumentStart"}) end,
+	[keych('G', true)] = "DocumentEnd",
+	[keych'0'] = "HomeDisplay",
+	[keych'k'] = "LineUp", 
+	[keych'j'] = "LineDown", 
+	[keych'b'] = "WordLeft", 
+	[keych'w'] = "WordRight", 
+	[keych'e'] = "WordRightEnd",
+	[keych'p'] = "Paste", 
+	[keych('$', true, false)] = "LineEnd",
+	[keych'h'] = "CharLeft",
+	[keych'l'] = "CharRight",
+	[keych'u'] = "Undo",
+	[keych('r',false, true)] = "Redo",
+	[keych('d',false, true)] = "PageDown",
+	[keych('u',false, true)] = "PageUp",
+	[keych('S', true)] = function() scite.MenuCommand(IDM_SAVE) end,
+	[keych'o'] = function() editor:LineEnd() editor:NewLine() iMode() end,
+	[keych('A', true)] = function() editor:LineEnd() iMode() end,
+	[keych'a'] = function() editor:CharRight() iMode() end,
+	[keych'x'] = function() editor:CharRight() editor:DeleteBack() end,
+	[keych'i'] = function() iMode() end
 }
 
 function nmapOnKey(key, shift, ctrl, alt)
-	local mod = ((shift and 1 or 0) << 2) | ((ctrl and 1 or 0) << 1) | (alt and 1 or 0)
-	local act = nmap[(key << 3) | mod]
+	local act = nmap[keycode(key, shift, ctrl, alt)]
 	if act then
 		if type(act) == "string" then
 			editor[act](editor)
@@ -39,8 +90,13 @@ function nmapOnKey(key, shift, ctrl, alt)
 end
 
 function imapOnKey(key, shift, ctrl, alt)
-	if ctrl and key == 91 then
-		OnKey = nmapOnKey
+	local act = imap[keycode(key, shift, ctrl, alt)]
+	if act then
+		if type(act) == "string" then
+			editor[act](editor)
+		else
+			act()
+		end
 		return true
 	end
 	return false
